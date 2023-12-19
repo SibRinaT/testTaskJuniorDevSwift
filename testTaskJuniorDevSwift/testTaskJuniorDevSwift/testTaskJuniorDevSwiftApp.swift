cbb7dc811642a2a7282a014fe18ac203fa03f39d
Ainur <sibrinat616@gmail.com>
2023-12-19 15:08:25 +0300

    init

---
+++ testTaskJuniorDevSwift/testTaskJuniorDevSwift/testTaskJuniorDevSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  testTaskJuniorDevSwiftApp.swift
+//  testTaskJuniorDevSwift
+//
+//  Created by Ainur on 19.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct testTaskJuniorDevSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
