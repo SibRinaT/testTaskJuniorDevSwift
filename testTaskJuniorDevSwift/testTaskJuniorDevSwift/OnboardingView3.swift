863de498be3e1783e1a058ccb65b6f0ed18e5d4a
Ainur <sibrinat616@gmail.com>
2024-01-31 11:41:58 +0300

    god damn

--- testTaskJuniorDevSwift/testTaskJuniorDevSwift/OnboardingView3.swift
+++ testTaskJuniorDevSwift/testTaskJuniorDevSwift/OnboardingView3.swift
@@ -1,36 +1,23 @@
 //
-//  SignUpView.swift
+//  OnboardingView3.swift
 //  testTaskJuniorDevSwift
 //
-//  Created by Ainur on 30.01.2024.
+//  Created by Ainur on 31.01.2024.
 //
 
 import SwiftUI
-struct OnBoardingStep {
-    let image: String
-    let title: String
-    let description: String
-}
-
-private let onBoardingSteps = [
-    OnBoardingStep(image: "onboardingImage1", title: "Quick Delivery At Your \n Doorstep", description: "Enjoy quick pick-up and delivery to \n your destination"),
-    OnBoardingStep(image: "onboardingImage2", title: "Flexible Payment", description: "Different modes of payment either \n before and after delivery \n without stress"),
-    OnBoardingStep(image: "onboardingImage2_1", title: "Real-time Tracking", description: "Track your packages/items from the \n comfort of your home till final destination")
-]
-struct OnboardingView: View {
+struct OnboardingView3: View {
     @State private var currentStep = 0
     
     var body: some View {
         VStack {
-            TabView(selection: $currentStep) {
-                ForEach(0..<onBoardingSteps.count) { it in
                     VStack {
-                        Image(onBoardingSteps[it].image)
-                        Text(onBoardingSteps[it].title)
+                        Image("onboardingImage2_1")
+                        Text("Real-time Tracking")
                             .foregroundColor(Color("MainColor"))
                             .multilineTextAlignment(.center)
                             .font(Font.title2.weight(.bold))
-                        Text(onBoardingSteps[it].description)
+                        Text("Track your packages/items from the \n comfort of your home till final destination")
                             .font(.callout)
                             .multilineTextAlignment(.center)
                             .foregroundColor(Color("blackColor"))
@@ -39,7 +26,7 @@ struct OnboardingView: View {
                         HStack {
                             //first button
                             Button(action: {
-                                self.currentStep = onBoardingSteps.count - 1
+                                
                             }) {
                                 Text("Skip")
                                     .bold()
@@ -56,11 +43,7 @@ struct OnboardingView: View {
                             .padding(60)
                             //second button
                             Button(action: {
-                                if self.currentStep < onBoardingSteps.count - 1 {
-                                    self.currentStep += 1
-                                }  else {
-                                    OnboardingView2()
-                                }
+                              
                             }) {
                                 Text("Next")
                                     .bold()
@@ -77,13 +60,10 @@ struct OnboardingView: View {
                             .padding(60)
                         }
                     }
-                    .tag(it)
                 }
             }
         }
-    }
-}
 
 #Preview {
-    OnboardingView()
+    OnboardingView3()
 }
