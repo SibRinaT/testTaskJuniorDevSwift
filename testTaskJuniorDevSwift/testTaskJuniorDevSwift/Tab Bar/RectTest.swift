f00b1a259e0de33801c4ea84829fcb7d1c8a0517
Ainur <sibrinat616@gmail.com>
2024-02-02 13:41:03 +0300

    fixed conflict with rectangleProfileView

---
+++ testTaskJuniorDevSwift/testTaskJuniorDevSwift/Tab Bar/RectTest.swift
@@ -0,0 +1,18 @@
+//
+//  RectTest.swift
+//  testTaskJuniorDevSwift
+//
+//  Created by Ainur on 02.02.2024.
+//
+
+import SwiftUI
+
+struct RectTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RectTest()
+}
