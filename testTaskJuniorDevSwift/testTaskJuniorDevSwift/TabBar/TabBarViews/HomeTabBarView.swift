b0a375c2fae7d378186de59ada79b8669c53ef50
Ainur <sibrinat616@gmail.com>
2024-02-04 17:55:51 +0300

    Home Tab bar started

--- testTaskJuniorDevSwift/testTaskJuniorDevSwift/TabBar/TabBarViews/HomeTabBarView.swift
+++ testTaskJuniorDevSwift/testTaskJuniorDevSwift/TabBar/TabBarViews/HomeTabBarView.swift
@@ -8,14 +8,63 @@
 import SwiftUI
 
 struct HomeTabBarView: View {
+    @State var user: User
+
     var body: some View {
         VStack {
+            ProfileView(user: user)
+            
+            Rectangle()
+                .cornerRadius(10)
+                .foregroundColor(Color("gray1"))
+                .frame(height: 116)
+                .padding(.horizontal)
+                .overlay(
+                    VStack{
+                        Text("Top Up")
+                            .bold()
+                        HStack {
+                            VStack {
+                                Image("BankIcon")
+                                Text("Bank")
+                            }
+                            VStack {
+                                Image("TransferIcon")
+                                Text("Transfer")
 
+                            }
+                            VStack {
+                                Image("CardIcon")
+                                Text("Card")
+                            }
+                        }
+                    }
+                )
+            VStack(alignment: .leading)
+             {
+                Text("Transaction History")
+                     .font(.title2)
+                     .bold()
+            }
+             //testRecnatgles
+            ScrollView {
+                RectangleProfileView(image: "BankIcon", title: "fadsfads", title2: "safddsaf")
+                RectangleProfileView(image: "BankIcon", title: "fadsfads", title2: "safddsaf")
+                RectangleProfileView(image: "BankIcon", title: "fadsfads", title2: "safddsaf")
+                RectangleProfileView(image: "BankIcon", title: "fadsfads", title2: "safddsaf")
+                RectangleProfileView(image: "BankIcon", title: "fadsfads", title2: "safddsaf")
+                RectangleProfileView(image: "BankIcon", title: "fadsfads", title2: "safddsaf")
+
+            }
 
         }
+        .navigationTitle("Home")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    HomeTabBarView()
+    NavigationView {
+        HomeTabBarView(user: User.mockUser)
+    }
 }
