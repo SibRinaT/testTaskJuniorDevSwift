7845de43b99946b49496242f1f5a436d2188ae26
Ainur <sibrinat616@gmail.com>
2024-02-07 15:49:08 +0300

    icons for sendpackage

--- testTaskJuniorDevSwift/testTaskJuniorDevSwift/TabBar/SendPackageViews/SendPackageView.swift
+++ testTaskJuniorDevSwift/testTaskJuniorDevSwift/TabBar/SendPackageViews/SendPackageView.swift
@@ -9,43 +9,69 @@ import SwiftUI
 
 struct SendPackageView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                Image("pointWithout")
-                Text("Origin Details")
-                    .font(.title3)
-            }
-            SendFieldView(fieldValue: "", placeholder: "Address")
-            SendFieldView(fieldValue: "", placeholder: "State,Country")
-            SendFieldView(fieldValue: "", placeholder: "Phone number")
-            SendFieldView(fieldValue: "", placeholder: "Others")
+        VStack {
+            ScrollView {
+                HStack {
+                    Image("pointWithout")
+                    Text("Origin Details")
+                        .font(.title3)
+                }
+                SendFieldView(fieldValue: "", placeholder: "Address")
+                SendFieldView(fieldValue: "", placeholder: "State,Country")
+                SendFieldView(fieldValue: "", placeholder: "Phone number")
+                SendFieldView(fieldValue: "", placeholder: "Others")
 
-            HStack {
-                Image("pointIcon")
-                Text("Origin Details")
-                    .font(.title3)
-            }
-            SendFieldView(fieldValue: "", placeholder: "Address")
-            SendFieldView(fieldValue: "", placeholder: "State,Country")
-            SendFieldView(fieldValue: "", placeholder: "Phone number")
-            SendFieldView(fieldValue: "", placeholder: "Others")
-            
-            HStack {
-                Image("addSquareIcon")
-                Text("Add destination")
-                    .foregroundColor(Color("gray1"))
-            }
+                HStack {
+                    Image("pointIcon")
+                    Text("Origin Details")
+                        .font(.title3)
+                }
+                SendFieldView(fieldValue: "", placeholder: "Address")
+                SendFieldView(fieldValue: "", placeholder: "State,Country")
+                SendFieldView(fieldValue: "", placeholder: "Phone number")
+                SendFieldView(fieldValue: "", placeholder: "Others")
+                
+                HStack {
+                    Image("addSquareIcon")
+                    Text("Add destination")
+                        .foregroundColor(Color("gray1"))
+                }
 
-            HStack {
-                Text("Origin Details")
-                    .font(.title3)
+                HStack {
+                    Text("Origin Details")
+                        .font(.title3)
+                }
+                SendFieldView(fieldValue: "", placeholder: "Address")
+                SendFieldView(fieldValue: "", placeholder: "State,Country")
+                SendFieldView(fieldValue: "", placeholder: "Phone number")
+                SendFieldView(fieldValue: "", placeholder: "Others")
             }
-            SendFieldView(fieldValue: "", placeholder: "Address")
-            SendFieldView(fieldValue: "", placeholder: "State,Country")
-            SendFieldView(fieldValue: "", placeholder: "Phone number")
-            SendFieldView(fieldValue: "", placeholder: "Others")
-            
+            HStack {
+                Rectangle()
+                    .shadow(radius: 2, x: 0, y: 2)
+                    .overlay (
+                        VStack {
+                            Image("clockIcon")
+                            Text("asd")
+                        }
+                        )
+                    .frame(width: 159, height: 75)
+                    .foregroundColor(.white)
+                
+                Rectangle()
+                    .shadow(radius: 2, x: 0, y: 2)
+                    .overlay (
+                        VStack {
+                            Image("calendarIcon")
+                            Text("asd")
+                        }
+                        )
+                    .frame(width: 159, height: 75)
+                    .foregroundColor(.white)
+            }
         }
+        .navigationTitle("Package")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
  
